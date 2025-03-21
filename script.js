d6b689c7c238bc192e19c39450559152fd494e71
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-03-21 12:29:28 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-.classList.add("score-bounce");
-    setTimeout(() => correctScoreEl.classList.remove("score-bounce"), 600);
-}tn.style.display = "block";
-}
-
-function restartGame() {
-    correc => {
-        quizContainer.style.display = "none";
+   quizContainer.style.display = "none";
         startScreen.style.display = "block";
         startScreen.classList.remove("fade-out");
         startScreen.classList.add("fade-in");
