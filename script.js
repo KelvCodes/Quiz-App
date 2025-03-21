0451cbaaaa6a762c0473b81b02a936801e0fee53
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-03-21 12:29:33 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-   quizContainer.style.display = "none";
-        startScreen.style.display = "block";
-        startScreen.classList.remove("fade-out");
-        startScreen.classList.add("fade-in");
-        categorySelect.value = "";
-    }, 500);
-}
+
